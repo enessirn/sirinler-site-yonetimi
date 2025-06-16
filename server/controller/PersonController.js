f95dcc81fe0eb6475a722b48ea6786baea427c3e
Enes Sirin <sefsirin55@gmail.com>
2025-06-16 12:13:51 +0300

    add aidat

--- server/controller/PersonController.js
+++ server/controller/PersonController.js
@@ -1,6 +1,6 @@
 const Person = require("../models/Person");
-
- const getAllPersons = async (req, res) => {
+const Transaction = require("../models/Transaction")
+const getAllPersons = async (req, res) => {
   try {
     const persons = await Person.find();
     res.status(200).json(persons);
@@ -9,31 +9,72 @@ const Person = require("../models/Person");
   }
 }
 
- const addPerson = async (req, res) =>  {
-    try {
-        const newPerson = new Person(req.body);
-        const savedPerson = await newPerson.save();
-        res.status(201).json(savedPerson);
-    } catch (error) {
-        res.status(500).json({ message: "Error adding person", error });
-    }
+const addPerson = async (req, res) => {
+  try {
+    const newPerson = new Person(req.body);
+    const savedPerson = await newPerson.save();
+    res.status(201).json(savedPerson);
+  } catch (error) {
+    res.status(500).json({ message: "Error adding person", error });
+  }
 }
 
 const deletePerson = async (req, res) => {
-    try {
-        const personId = req.params.id;
-        const deletedPerson = await Person.findByIdAndDelete(personId);
-        if (!deletedPerson) {
-            return res.status(404).json({ message: "Person not found" });
-        }
-        res.status(200).json({ message: "Person deleted successfully" });
-    } catch (error) {
-        res.status(500).json({ message: "Error deleting person", error });
+  try {
+    const personId = req.params.id;
+    const deletedPerson = await Person.findByIdAndDelete(personId);
+    if (!deletedPerson) {
+      return res.status(404).json({ message: "Person not found" });
     }
+    res.status(200).json({ message: "Person deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting person", error });
+  }
+}
+
+const addAidat = async (req, res) => {
+  try {
+    const personId = req.params.id;
+    const date = new Date();
+    if (!personId) return res.status(500).json({ message: "Kişi Bulunamadı" })
+    const personAidat = await Person.findByIdAndUpdate(
+      personId,
+      {
+        $set: {
+          "aidat": true,
+          "date": date
+
+        }
+      },
+      { new: true }
+    );
+
+    // update transaction
+    const months = ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"]
+    const getPerson = await Person.findById(personId);
+    if(!getPerson) return res.status(500).json({message: "Kişi bulunamadı"})
+    const newTransaction = new Transaction({
+      "title": "Aidat Ödemesi",
+      "desc": `${getPerson.fullName} ${months[date.getMonth()]} Ayı Aidat Ödemesi`,
+      "amount": 200,
+      "date": new Date(),
+      "type": "gelir"
+
+    })
+    await newTransaction.save();
+    res.status(201).json({
+      message: "Aidat güncellendi",
+      updatedPerson: personAidat,
+      transaction: newTransaction
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error when uptade the person's aidat", error });
+  }
 }
 
 module.exports = {
   getAllPersons,
   addPerson,
-  deletePerson
+  deletePerson,
+  addAidat
 };
--- server/routes/personRoutes.js
+++ server/routes/personRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { getAllPersons, addPerson, deletePerson } = require('../controller/PersonController');
+const { getAllPersons, addPerson, deletePerson, addAidat } = require('../controller/PersonController');
 const router = express.Router();
 
 router.get('/', getAllPersons);
 router.post('/add', addPerson);
 router.delete('/delete/:id', deletePerson);
-
+router.post('/add-aidat/:id', addAidat)
 module.exports = router;
