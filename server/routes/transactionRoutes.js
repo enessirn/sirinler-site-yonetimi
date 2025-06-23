e60d5c68fc46de438a9da0aff6fe8ff7b5b678c1
Enes Sirin <sefsirin55@gmail.com>
2025-06-23 22:57:10 +0300

    delete all transactions

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllTransactions, addTransaction, deleteTransaction, getAllAmounts } = require('../controller/transactionController');
+const { getAllTransactions, addTransaction, deleteTransaction, getAllAmounts, deleteAllTransaction } = require('../controller/transactionController');
 const router = express.Router();
 
 router.get('/', getAllTransactions);
@@ -7,4 +7,5 @@ router.post('/add', addTransaction);
 router.delete('/delete/:id', deleteTransaction);
 
 router.get('/amounts', getAllAmounts);
+router.delete('/delete-all-transaction', deleteAllTransaction);
 module.exports = router;
